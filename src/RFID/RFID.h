2d1569fe0c3b870d24681df98452a9be94e535f3
aitor <aitor.mendiburu@ikasle.aeg.eus>
2024-10-28 00:10:39 +0100

    [MENDI-275][ADD]move RFI logic to new folder

--- src/RFID/RFID.h
+++ src/RFID/RFID.h
@@ -9,3 +9,16 @@ void printHex(byte *buffer, byte bufferSize);
 void printDec(byte *buffer, byte bufferSize);
 
 #endif
+
+
+// // rfidHandler.h
+// #ifndef RFIDHANDLER_H
+// #define RFIDHANDLER_H
+
+// #include <MFRC522.h>
+
+// void setupRFID(MFRC522 &rfid);
+// bool readRFIDCard(MFRC522 &rfid, String &cardID);
+
+// #endif
+
