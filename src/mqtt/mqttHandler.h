49b0e88a15a7cec8eed667ad3bf93d4689c05562
aitor <aitor.mendiburu@ikasle.aeg.eus>
2024-10-28 00:12:14 +0100

    [MENDI-275][ADD]move mqtt logic to new file

---
+++ src/mqtt/mqttHandler.h
@@ -0,0 +1,13 @@
+// mqttHandler.h
+#ifndef MQTTHANDLER_H
+#define MQTTHANDLER_H
+
+#include <PubSubClient.h>
+
+// Function declarations
+void setupMQTT(PubSubClient &client);
+void mqttReconnect(PubSubClient &client);
+void handleMQTTMessage(char *topic, byte *message, unsigned int length);
+void publishDoorStatus(PubSubClient &client, const char *status);
+
+#endif
